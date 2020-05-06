36cc73deb6e60e6666cdeab7af8d980e1f367821
lfernandez55 <lfernandez@weber.edu>
2020-05-06 00:46:12 -0600

    Commit

---
+++ resources/costello.js
@@ -0,0 +1,15 @@
+function calculateOrder() {
+
+// TODO:  Calculate order and display it
+
+
+}
+
+function validateAndSubmitForm() {
+
+  // TODO: Validate form and display validation errors if any
+  // otherwise submit form
+
+  // for the form to actually submit, change from return false to return true:
+  return false;
+}
